87e790ad14c508ea36dfeb68b58625bae64ad9dd
SergeiGluhov <166560298+SergeiGluhov@users.noreply.github.com>
2024-04-10 07:01:41 +0700

    Небольшие правки, добавил комментарии

--- src/Figure.py
+++ src/Figure.py
@@ -1,11 +1,8 @@
-# создать фигуру
-# добавить фигуру в массив
-# повернуть фигуру
-# можно ли повернуть фигуру
 import random
 
 
 class Figure:
+    #Массив представляющий собой поле
     mas = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -25,19 +22,23 @@ class Figure:
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]           ]
+           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
+    #Переменная для подсчета очков
     score = 0
+    #Переменная которая сообщает закончилась игра или еще идет
+    #False - игра еще не закончена
+    #True - игра закончилась
     endGame = False
-    nameFigure = None
-    typeFigure = None
 
-
+    #Перменными Tu Tr Td Tl обозначаются массивы обозначающие фигуры,
+    # где 0 - пусто 1 - заполненно
     Tu = [[0, 1, 0], [1, 1, 1]]
     Tr = [[1, 0], [1, 1], [1, 0]]
     Td = [[1, 1, 1], [0, 1, 0]]
     Tl = [[0, 1], [1, 1], [0, 1]]
-    T = [Tu, Tr, Td, Tl]  # Положение фигур
+    # Переменной T обозначается фигура и массив ее положений
+    T = [Tu, Tr, Td, Tl]
 
     Ou = [[1, 1], [1, 1]]
     O = [Ou]
@@ -52,113 +53,129 @@ class Figure:
     Jd = [[1, 1, 1], [0, 0, 1]]
     J = [Jl, Ju, Jr, Jd]
 
-    Zh =[[1,1,0],[0,1,1]]
-    Zv =[[0,1], [1,1], [1,0]]
+    Zh = [[1, 1, 0], [0, 1, 1]]
+    Zv = [[0, 1], [1, 1], [1, 0]]
     Z = [Zh, Zv]
 
-    Sh = [[0,1,1],[1,1,0]]
-    Sv = [[1,0],[1,1],[0,1]]
+    Sh = [[0, 1, 1], [1, 1, 0]]
+    Sv = [[1, 0], [1, 1], [0, 1]]
     S = [Sh, Sv]
 
-    Lr = [[1,0],[1,0],[1,1]]
-    Ld = [[1,1,1], [1,0,0]]
-    Ll = [[1,1],[0,1],[0,1]]
-    Lu = [[0,0,1],[1,1,1]]
+    Lr = [[1, 0], [1, 0], [1, 1]]
+    Ld = [[1, 1, 1], [1, 0, 0]]
+    Ll = [[1, 1], [0, 1], [0, 1]]
+    Lu = [[0, 0, 1], [1, 1, 1]]
     L = [Lr, Ld, Ll, Lu]
-    figure = [T, O, I, J, Z, S, L]  # Хранит имена фигур
 
+    #Список для хранения фигур
+    figure = [T, O, I, J, Z, S, L]
+
+    #Обозначает индекс фигуру в массиве figure
+    nameFigure = None
+    #Обозначает индекс положения фигуры в подмассивах T, O, I, J, Z, S, L
+    typeFigure = None
+    #Генерируем следующую фигуру
     nameNextFigure = random.randint(0, len(figure) - 1)
     typeNextFigure = random.randint(0, len(figure[nameNextFigure]) - 1)
 
+    #Создаем случайным образом и случайным образом задаем ей положение,
+    #так же генерируем следующую фигуру
     def createFigure(self):
         self.nameFigure = self.nameNextFigure
         self.typeFigure = self.typeNextFigure
-        self.nameNextFigure = random.randint(0, len(self.figure)-1)
-        self.typeNextFigure = random.randint(0, len(self.figure[self.nameNextFigure])-1)
+        self.nameNextFigure = random.randint(0, len(self.figure) - 1)
+        self.typeNextFigure = random.randint(0, len(self.figure[self.nameNextFigure]) - 1)
 
+    #Очищаем массив
     def emptyField(self):
         for i in range(0, len(self.mas)):
             for j in range(0, len(self.mas[i])):
                 self.mas[i][j] = 0
         self.score = 0
+
+    #Проверяет собрался ряд или нет
     def checkCompleted(self):
         mas = self.mas
+        #Считаем сколько строк собралось
         countScore = 0
         for i in range(0, len(mas)):
+            #Флаг который говорит, что строка собралась, если встретим 0 или 1,
+            # что означает что строка не полностью собрана, то меняем на False
             flag = True
             for j in range(0, len(mas[i])):
                 if mas[i][j] == 0 or mas[i][j] == 1:
                     flag = False
-            if flag == True:
+            #Если строка собралась, то удаляем ее, в начало массива
+            # добавляем новую пустую строку и считаем ее в перменной countScore
+            if flag:
                 zeros_list = [0] * len(mas[0])
                 mas.pop(i)
                 mas.insert(0, zeros_list)
                 countScore = countScore + 1
 
+        #Проверяем сколько собралось строк и прибавляем очки
         if countScore == 1:
             self.score = self.score + 100
-        if countScore == 2:
+        elif countScore == 2:
             self.score = self.score + 300
-        if countScore == 3:
+        elif countScore == 3:
             self.score = self.score + 700
-        if countScore == 4:
+        elif countScore == 4:
             self.score = self.score + 1500
 
-
+    #Делаем шаг вниз и проверяем можно ли его сделать,
+    #если нельзя то добавляем новую фигуру
     def stepDown(self):
         indRow = self.indRow + 1
         result = self.isCorrectPosition(indRow, self.indColumn, self.typeFigure)
+        #Если фигуру нельзя больше отпустить
         if result == False:
+            #Проверяем собрался ли ряд
             self.completedFigure()
+            #Проверяем установилась ли фигура
             self.checkCompleted()
+            #Создаем новую фигуру и добавляем в начало массива
             self.createFigure()
             self.addFigure()
+            #Считаем очки за установленную фигуру
             self.score = self.score + 10
             return
         self.cleanFigure()
         self.drawFigure()
 
+    #Проверяет возможность задать новую позицию фигуре
+    #вправо, влево или вниз
     def isCorrectPosition(self, indRow, indColumn, typeFigure):
         mas = self.figure[self.nameFigure][typeFigure]
 
+        #Задаем индексы для массива фигуры
         block_row, block_col = 0, 0
 
-        if indRow < 0:
-            #print("Отрисовать нельзя")
+        #Дальше проверяем условия по которым понимаем можно ли сдвинуть фигуру в массиве
+        if indColumn < 0 or indColumn + len(mas[0]) > len(self.mas[0]):
             return False
 
-        if indColumn >= 0 and indColumn + len(mas[0]) <= len(self.mas[0]):
-            #print("Отрисовать можно")
-            pass
-        else:
-            #print("Отрисовать нельзя")
-            return False
-
-        if indRow + len(mas) <= len(self.mas):
-            #print("Отрисовать можно")
-            pass
-        else:
-            #print("Отрисовать нельзя")
+        if indRow < 0 or indRow + len(mas) > len(self.mas):
             return False
 
         for i in range(indRow, len(mas) + indRow):
             for j in range(indColumn, len(mas[block_row]) + indColumn):
                 if mas[block_row][block_col] == 1 and self.mas[i][j] == 2:
-                    #print("Вставлять нельзя")
                     return False
                 block_col = block_col + 1
             block_col = 0
             block_row = block_row + 1
-        #print("Можно вставить")
 
         self.typeFigure = typeFigure
         self.indRow = indRow
         self.indColumn = indColumn
         return True
 
+    #Функция добавляет фигуру в начало массива
     def addFigure(self):
         mas = self.figure[self.nameFigure][self.typeFigure]
 
+        #Задаем столбец и строку в которой отрисуем фигуру, в зависимости от размера фигуры смещаем
         indRow, indColumn = 0, 0
         if len(mas[0]) == 4:
             indColumn = int((len(self.mas[0]) - 1) / 2) - 1
@@ -167,6 +184,7 @@ class Figure:
         if len(mas[0]) == 2 or len(mas[0]) == 1:
             indColumn = int((len(self.mas[0]) - 1) / 2)
 
+        #Проверяем можно ли вставить фигуру в начало, если позиция не корректная, то завершаем игру
         result = self.isCorrectPosition(indRow, indColumn, self.typeFigure)
         if result == False:
             self.endGame = True
@@ -175,6 +193,7 @@ class Figure:
         # Отрисовываем фигуру
         self.drawFigure()
 
+    #Функция смещает фигуру вправо
     def stepRight(self):
         indColumn = self.indColumn + 1
         result = self.isCorrectPosition(self.indRow, indColumn, self.typeFigure)
@@ -184,6 +203,7 @@ class Figure:
         self.cleanFigure()
         self.drawFigure()
 
+    # Функция смещает фигуру влево
     def stepLeft(self):
         indColumn = self.indColumn - 1
         result = self.isCorrectPosition(self.indRow, indColumn, self.typeFigure)
@@ -192,6 +212,7 @@ class Figure:
         self.cleanFigure()
         self.drawFigure()
 
+    #Фунция рисует фигуру в массиве
     def drawFigure(self):
         mas = self.figure[self.nameFigure][self.typeFigure]
 
@@ -205,23 +226,23 @@ class Figure:
             block_col = 0
             block_row = block_row + 1
 
+    #Очищаем поле от положения старой фигуры
     def cleanFigure(self):
         for i in range(0, len(self.mas)):
             for j in range(0, len(self.mas[i])):
                 if self.mas[i][j] == 1:
                     self.mas[i][j] = 0
 
+    #Функция задает окончательное положение фигуре на поле
     def completedFigure(self):
         for i in range(0, len(self.mas)):
             for j in range(0, len(self.mas[i])):
                 if self.mas[i][j] == 1:
                     self.mas[i][j] = 2
 
-    # кардинаты зделать
-    # кардинату хранить в фигуре
-    # функция добавить фигуру в м асив
-    # определить кардинаты и хранить их в фигуре
+    #Функция поворачивает фигуру
     def rotateFigure(self):
+        #Проверяем можно ли задать фигуре следующее положение, если нельзя возвращаем в первое
         if len(self.figure[self.nameFigure]) > self.typeFigure + 1:
             typeFigure = self.typeFigure + 1
         else:
@@ -230,6 +251,7 @@ class Figure:
         indColumn = self.indColumn
         indRow = self.indRow
 
+        #Каждая фигура и ее варианты имеют свой центр, смещаем координаты для установки на поле
         if self.nameFigure == 0 and typeFigure == 1:
             indColumn = indColumn + 1
         if self.nameFigure == 0 and typeFigure == 2:
@@ -258,14 +280,14 @@ class Figure:
             indColumn = indColumn + 1
         if self.nameFigure == 4 and typeFigure == 0:
             indColumn = indColumn - 1
-            indRow = indRow +1
+            indRow = indRow + 1
 
         if self.nameFigure == 5 and typeFigure == 1:
             indRow = indRow - 1
             indColumn = indColumn + 1
         if self.nameFigure == 5 and typeFigure == 0:
             indColumn = indColumn - 1
-            indRow = indRow +1
+            indRow = indRow + 1
 
         if self.nameFigure == 6 and typeFigure == 1:
             indRow = indRow + 1
@@ -282,6 +304,7 @@ class Figure:
         self.cleanFigure()
         self.drawFigure()
 
+    #Выводит массив фигуры в консоль для отладки
     def printFigure(self):
         fig = self.figure[self.nameFigure][self.typeFigure]
         for i in range(0, len(fig)):
@@ -290,10 +313,10 @@ class Figure:
             print()
         print()
 
+    #Выводит массив поля в консоль для отладки
     def printMas(self):
         for i in self.mas:
             for j in i:
                 print(j, end=" ")
             print()
         print()
-
--- src/Interface.py
+++ src/Interface.py
@@ -4,13 +4,18 @@ from pygame.locals import *
 
 from src.Figure import Figure
 
-
+#Класс для рисования графического интерфейса приложения
 class Interface:
+    #Задаем сколько раз обновится дисплей за 1 секунду
     fps = 60
+    #Задаем размер окна
     window_w, window_h = 380, 468
+    #Используем для отслеживания какая была нажата клавиша
     step_right, step_left, step_down, step_space = 0, 0, 0, 0
+    #Используем для отслеживания как долго была нажата клавиша
     right_time, left_time, down_time, space_time = 0, 0, 0, 0
 
+    #Устанавливаем параметры окна
     def initPygame(self):
         global fps_clock, display_surf, basic_font, big_font
         pg.init()
@@ -24,6 +29,7 @@ class Interface:
 
         pg.display.set_caption('Hell TETRIS')
 
+    #Функция для запуска программы
     def start(self):
         self.initPygame()
         self.initInterface()
@@ -32,16 +38,21 @@ class Interface:
         figure = Figure()
         figure.createFigure()
         figure.addFigure()
-        self.drafNextFigure(figure)
+        self.drawNextFigure(figure)
         pg.display.update()
+
         last_move_down = time.time()
         while True:
-            self.updateField(figure.mas)
-            if (time.time() - last_move_down > 0.3):
+            # Задаем время через которое фигура будет смещаться в низ
+            if time.time() - last_move_down > 0.3:
+                # Смещаем фигуру
                 figure.stepDown()
-                pg.display.update()
+                # Выводим массив на экран
+                self.updateField(figure.mas)
+                # Обновляем время для следующего замера
                 last_move_down = time.time()
 
+            # Отслеживаем какая клавиша была нажата и запоминаем время начала нажатия
             for event in pg.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_RIGHT:
@@ -56,8 +67,9 @@ class Interface:
                     if event.key == K_SPACE:
                         self.step_space = True
                         self.space_time = time.time()
-                    elif event.type == pg.QUIT:
-                        exit()
+                if event.type == pg.QUIT:
+                    exit()
+                # Если клавишу отпустили меняем состояние
                 if event.type == KEYUP:
                     if event.key == K_RIGHT:
                         self.step_right = False
@@ -68,34 +80,48 @@ class Interface:
                     if event.key == K_SPACE:
                         self.step_space = False
 
+            # Проверяем была ли нажата клавиша и как долго если она удерживается,
+            # если удерживается долго то, считаем, что нажимается несколько раз
             if self.step_right and time.time() - self.right_time > 0.1:
                 figure.stepRight()
+                self.updateField(figure.mas)
                 self.right_time = time.time()
             if self.step_left and time.time() - self.left_time > 0.1:
                 figure.stepLeft()
+                self.updateField(figure.mas)
                 self.left_time = time.time()
             if self.step_down and time.time() - self.down_time > 0.1:
                 figure.stepDown()
+                self.updateField(figure.mas)
+                self.drawNextFigure(figure)
                 self.down_time = time.time()
             if self.step_space and time.time() - self.space_time > 0.1:
                 figure.rotateFigure()
+                self.updateField(figure.mas)
                 self.space_time = time.time()
 
+            # Выводим на экран количество набранных очков
             display_surf.fill((158, 173, 134), (300, 10, 70, 20))
             score = basic_font.render(str(figure.score).rjust(6, "0"), 3, (0, 0, 0))
             display_surf.blit(score, (300, 10))
 
-            if figure.endGame == True:
+            #Проверяем закончилась игра или нет
+            if figure.endGame:
+                # Выводим заставку обновления поля
                 self.screeSaver()
+                # Очищаем поле
                 figure.emptyField()
+                # Создаем новую фигуру и добавляем на поле
                 figure.createFigure()
                 figure.addFigure()
+                self.drawNextFigure(figure)
+                self.updateField(figure.mas)
+                # Задаем флаг начало новой игры
                 figure.endGame = False
 
-            self.drafNextFigure(figure)
-            pg.display.update()
-            fps_clock.tick(self.fps)
 
+
+    # Функция отрисовывает интерфейс программы
     def initInterface(self):
         # Главное поле
         pg.draw.rect(display_surf, (0, 0, 0), (5, 5, 228, 448), 2)
@@ -109,12 +135,13 @@ class Interface:
 
         self.screeSaver()
 
-    def drafNextFigure(self, figure):
+    # Отрисовываем следующую фигуру на мини поле
+    def drawNextFigure(self, figure):
         mas = figure.figure[figure.nameNextFigure][figure.typeNextFigure]
         self.showEmptyFieldNextFigure()
         self.updateFieldNextFigure(mas)
 
-
+    # Заставка разделяющая начало и конец игры
     def screeSaver(self):
         timeLoading = time.time()
         countBlock = 0
@@ -140,16 +167,20 @@ class Interface:
                 timeLoading = time.time()
 
             pg.display.update()
+
+    # Показываем пустое поле в начале игры и в конце
     def showEmptyField(self):
         for i in range(0, 20):
             for j in range(0, 10):
                 self.emptyBlock(i, j)
 
+    # Отрисовываем фигуру в мини поле при каждой новой фигуре
     def showEmptyFieldNextFigure(self):
         for i in range(0, 4):
             for j in range(0, 4):
                 self.emptyBlockNextFigure(i, j)
 
+    # Показываем пустое мини поле при каждой новой фигуре
     def updateFieldNextFigure(self, mas):
         for i in range(0, len(mas)):
             for j in range(0, len(mas[i])):
@@ -158,6 +189,7 @@ class Interface:
                 else:
                     self.emptyBlockNextFigure(i, j)
 
+    # Обновляем поле данными из массива
     def updateField(self, field):
         for i in range(0, 20):
             for j in range(0, 10):
@@ -165,23 +197,29 @@ class Interface:
                     self.fillBlock(i, j)
                 else:
                     self.emptyBlock(i, j)
+        pg.display.update()
+        fps_clock.tick(self.fps)
 
+    # Выводим пустой блок на большом поле
     def emptyBlock(self, indRow, indCol):
         pg.draw.rect(display_surf, (135, 147, 114), (10 + 22 * indCol, 10 + 22 * indRow, 20, 20), 1)
         pg.draw.rect(display_surf, (135, 147, 114), (14 + 22 * indCol, 14 + 22 * indRow, 12, 12))
 
+    # Выводим заполненный блок на большом поле
     def fillBlock(self, indRow, indCol):
         pg.draw.rect(display_surf, (0, 0, 0), (10 + 22 * indCol, 10 + 22 * indRow, 20, 20), 1)
         pg.draw.rect(display_surf, (0, 0, 0), (14 + 22 * indCol, 14 + 22 * indRow, 12, 12))
 
+    # Выводим пустой блок на мини поле
     def emptyBlockNextFigure(self, indRow, indCol):
         pg.draw.rect(display_surf, (135, 147, 114), (270 + 22 * indCol, 100 + 22 * indRow, 20, 20), 1)
         pg.draw.rect(display_surf, (135, 147, 114), (274 + 22 * indCol, 104 + 22 * indRow, 12, 12))
 
+    # Выводим полный блок на мини поле
     def fillBlockNextFigure(self, indRow, indCol):
         pg.draw.rect(display_surf, (0, 0, 0), (270 + 22 * indCol, 100 + 22 * indRow, 20, 20), 1)
         pg.draw.rect(display_surf, (0, 0, 0), (274 + 22 * indCol, 104 + 22 * indRow, 12, 12))
 
-
+# Запускаем приложение
 ui = Interface()
 ui.start()
